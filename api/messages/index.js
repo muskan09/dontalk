a7c212bd1da6433d0b64239cefde83f9abcaf107
hsangalli <habyte@gmail.com>
2018-01-24 08:51:25 -0200

    Added mongoose-paginate to the project dependencies

--- api/messages/index.js
+++ api/messages/index.js
@@ -10,11 +10,14 @@ router.post('/', (req, res) => {
   })
 })
 
-router.get('/:chat', (req, res) => {
-  Message.find({ chat: req.params['chat'] }, (err, messages) => {
-    if (err) res.send(err)
-    res.status(200).send(messages)
-  })
+router.get('/:chat/:page', (req, res) => {
+  Message.paginate({}, {
+      limit: 25,
+      page: req.params['page'],
+      sort: { _id: -1 }
+    }).then(response => {
+      res.status(200).send(response)
+    })
 })
 
 module.exports = router
--- api/messages/model.js
+++ api/messages/model.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate')
 
 const MessageSchema = new mongoose.Schema({
   chat: String,
   author: String,
   content: String
 })
+
+MessageSchema.plugin(mongoosePaginate)
 
 module.exports = mongoose.model('Message', MessageSchema)
