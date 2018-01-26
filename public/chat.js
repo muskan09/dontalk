131e816080f81589f2a01391b36a64d0251c9038
Hildor Junior <hildor.faber@acad.pucrs.br>
2018-01-26 11:08:30 -0200

    Sound again

--- public/chat.js
+++ public/chat.js
@@ -28,17 +28,17 @@ new Vue({
       }
     },
     getMessages: function (page = 1) {
-     // const lastMessageId = this.messages[0]._id;
-      console.log(this.messages);
+      if (this.messages && this.messages.length > 0) {
+        const lastMessageId = this.messages[this.messages.length-1]._id;
+      }
       this.page = page;
       this.$http.get(`/api/messages/${this.chat}/${page}`)
         .then(function (response) {
           this.messages = response.body.docs.slice().reverse();
           this.pages = response.body.pages;
-          //if (this.messages[0]._id != lastMessageId) {
-            //this.playSound();
-          //}
-        console.log(this.messages);
+          if (this.messages[this.messages.length-1]._id != lastMessageId) {
+            this.playSound();
+          }
         });
     },
     saveUser: function () {
